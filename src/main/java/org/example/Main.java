1f3e7f14c39ca1c9e2d92d38d67eaf19f09c9fff
darw182 <darwin.cachimil@epn.edu.ec>
2024-07-02 19:57:28 -0500

    segundo commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -4,21 +4,39 @@ import java.util.Scanner;
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Intro with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.println("Bienvenidos al Sistema de Prácticas Preprofesionales");
-        System.out.println("\t Menú");
-        System.out.println("1. Tutor");
-        System.out.println("2. Estudiante");
-        System.out.println("3. Administrador");
-        System.out.println("Ingrese el perfil de usuario: ");
-        Scanner opcion=new Scanner(System.in);
-        int op=opcion.nextInt();
-        if (op==1){
-            System.out.println("\t TUTOR");
-            System.out.println("Ingrese su nombre: ");
-            String nombre=opcion.nextLine();
+
+        Scanner opcion = new Scanner(System.in);
+
+
+        boolean aux = true;
+        while (aux) {
+            System.out.println("Bienvenidos al Sistema de Prácticas Preprofesionales");
+            System.out.println("\t Menú");
+            System.out.println("1. Tutor");
+            System.out.println("2. Estudiante");
+            System.out.println("3. Administrador");
+            System.out.println("4. Salir");
+            System.out.println("Ingrese el perfil de usuario: ");
+            int op = opcion.nextInt();
+
+            opcion.nextLine();
+            if (op == 1) {
+                System.out.println("*****************************");
+                System.out.println("\tTUTOR");
+                System.out.println("Ingrese su nombre: ");
+                String nombre = opcion.nextLine();
+            } else if (op == 2) {
+                System.out.println("*****************************");
+                System.out.println("\tEstudiante");
+                System.out.println("Ingrese su nombre: ");
+                String nombreEst = opcion.nextLine();
+            }
+
+            if (op == 4) {
+                aux = false;
+            }
         }
 
-        }
+
     }
+}
